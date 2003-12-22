582f347589287223fb0f622638a0890b396cdad8
noahm <noahm@97c37273-b125-0410-ac70-d172888cb9e0>
2003-12-22 20:11:54 +0000

    define htons and friends
    git-svn-id: file:///home/noahm/src/tmp/iputils/trunk@58 97c37273-b125-0410-ac70-d172888cb9e0

--- include-glibc/netinet/in.h
+++ include-glibc/netinet/in.h
@@ -8,4 +8,22 @@
 
 #define SOL_IP	0
 
+#include <stdint.h>
+
+/* Functions to convert between host and network byte order.
+
+   Please note that these functions normally take `unsigned long int' or
+   `unsigned short int' values as arguments and also return them.  But
+   this was a short-sighted decision since on different systems the types
+   may have different representations but the values are always the same.  */
+
+extern uint32_t ntohl (uint32_t __netlong) __THROW __attribute__ ((__const__));
+extern uint16_t ntohs (uint16_t __netshort)
+     __THROW __attribute__ ((__const__));
+extern uint32_t htonl (uint32_t __hostlong)
+     __THROW __attribute__ ((__const__));
+extern uint16_t htons (uint16_t __hostshort)
+     __THROW __attribute__ ((__const__));
+
+
 #endif	/* netinet/in.h */
